0243dee22d78ea29486b52c4802900278b097cd1
Rahul Soni <rahul.soni@spit.ac.in>
2020-07-26 00:54:22 +0530

    Accepting Normal input

---
+++ CFG/InputForm/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from InputForm.models import *
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+from CFG.settings import BASE_DIR, TEMPLATES
+@csrf_exempt
+def addcomplaint(request):
+    print("Hello")    
+    print(BASE_DIR)    
+    print(TEMPLATES[0]['DIRS'])
+    if request.method == "POST":
+        input1  = request.POST.get('input1')
+        input2  = request.POST.get('input2')
+        input3  = request.POST.get('input3')
+        input4  = request.POST.get('input4') 
+        return HttpResponse("Form submitted")
+    return render(request, "InputForm/home.html")
